0d054d14563be075e9f5b22165e23f205d73195a
chu9741 <chu9741@gmail.com>
2023-01-27 00:00:34 +0900

    q2 버블정렬

---
+++ Inflearn_Java_Algorithm/Ch6_Sorting_and_Searching/q2/Main.java
@@ -0,0 +1,34 @@
+package Inflearn_Java_Algorithm.Ch6_Sorting_and_Searching.q2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public int[] Solution(int n, int[] arr){
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp=arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) throws Exception{
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int a : main.Solution(n,arr))
+            System.out.print(a+" ");
+    }
+}
