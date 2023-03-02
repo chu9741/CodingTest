f3158368fd46b60de5ec183cb90d3e3db4036a2a
chu9741 <95557557+chu9741@users.noreply.github.com>
2023-03-02 17:27:04 +0900

    [level 2] Title: H-Index, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        int max=0;
+        
+        for(int c : citations){
+            max=Math.max(max,c);
+        }
+        
+        for(int i=0; i<max; i++){
+            int count=0;
+            for(int j : citations){
+                if(i<=j){
+                    count++;
+                }
+            }
+            // System.out.println(count);
+            if(count>=i)
+                answer=Math.max(i,answer);      
+        }
+        
+        //cit에 있는 값들은 인용횟수를 담았고 그리고 length는 논문의 수 
+        
+        return answer;
+    }
+}
